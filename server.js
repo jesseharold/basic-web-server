f0f820cf84195ad0e3277488b60599c9af15a29a
jesseharold <jesseharold@gmail.com>
2016-12-01 22:32:29 -0800

    changing to persist data in a file

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 var express = require("express");
+var bodyparser = require("simple-bodyparser");
+var fs = require("fs");
+var turtlesLoaded = require("./data/turtles.json");
+
 var app = express();
+app.use(bodyparser());
 
 // Add headers
 app.use(function (req, res, next) {
@@ -27,31 +32,49 @@ var turtles =  {
       data: [
         {
           name: "Raphael",
-          favFood: "Pizza"
+          favFood: "Pizza",
+          image: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ0OyXC-1HK9Mi0QucK7Svt-B7gkBHA4eXUfw39Sd77ggFsHF7p"
         },
         {
           name: "Leonardo",
-          favFood: "Pizza"
+          favFood: "Pizza",
+          image: "http://cdn1.bigcommerce.com/n-ou1isn/ydriczk/products/86454/images/86011/leonardo_TMNT_face_mask_buy_star_masks_at_starstills__65394__25495.1394515286.500.659.jpg?c=2"
         },
         {
           name: "Donatello",
-          favFood: "Pizza"
+          favFood: "Pizza",
+          image: "https://s-media-cache-ak0.pinimg.com/736x/88/6e/7f/886e7f2705900c2bb644ea78fd28adf7.jpg"
         },
         {
           name: "Michelangelo",
-          favFood: "Pizza"
+          favFood: "Pizza",
+          image: "https://s-media-cache-ak0.pinimg.com/736x/35/b8/25/35b82586c7143f417af5037c66cf9879.jpg"
         }
       ]
     };
 
-app.get("/turtles", function (req, res) {
-    res.send(turtles);
+app.get("/turtles", function (request, response) {
+    response.send(turtles);
 });
-app.post("/turtles", function (req, res) {
-    console.log(req.body.name);
-    for (var key in req){
-        //console.log(key);
+app.post("/turtles", function (request, response) {
+    var postData = request.body.split("&");
+    var newTurtle = {};
+    for (var i = 0; i < postData.length; i++){
+      var property = postData[i].split("=");
+      newTurtle[property[0]] = property[1];
     }
+    turtles.data.push(newTurtle);
+    writeDataToServer(turtles);
+    var responseData = {
+        message: "success",
+        data: newTurtle
+    };
+    response.send(responseData);
 });
+
 app.listen(3000);
 console.log("Server started at http://localhost:3000/");
+
+function writeDataToServer(data){
+  fs.writeFile("data/turtles.json", JSON.stringify(data));
+}
